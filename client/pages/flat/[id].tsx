8483eed1198c11626e4af3a8e03edbb7085afd8c
Vojtech Vanek <vanevo00@gmail.com>
2020-03-12 17:15:28 +0100

    feat: flat detail start

---
+++ client/pages/flat/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const flat = () => {
+  return (
+    <Layout>
+      flat
+    </Layout>
+  )
+};
+
+  export default flat;
