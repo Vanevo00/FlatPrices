2a6bc72bcba3082e8b94353e59b199bc292f1c15
Vojtech Vanek <vanevo00@gmail.com>
2020-06-19 15:00:07 +0200

    external image upload backend

--- server/routes/images.ts
+++ server/routes/images.ts
@@ -1,24 +1,17 @@
 import express, { Request, Response } from 'express'
+import * as fs from 'fs'
+import * as https from 'https'
 
 const router = express.Router()
 const multer = require('multer')
 const sharp = require('sharp')
-const path = require('path')
-
-// const imageStorage = multer.diskStorage(
-//   {
-//     destination: 'client/public/images',
-//     filename: ( req, file, cb ) => {
-      //get rid of diacritics, file extension, replace spaces with underscore
-      // const normalizedName = file.originalname.split(' ').join('_').normalize("NFD").replace(/[\u0300-\u036f]/g, '').replace(/\.[^/.]+$/, '')
-      // cb(null, normalizedName + Date.now() + path.extname(file.originalname));
-//     }
-//   }
-// )
+const axios = require ('axios')
 var imageStorage = multer.memoryStorage()
 
 const imageUpload = multer({ storage: imageStorage })
 
+// @route  POST api/images
+// @desc   upload and resize an image
 router.post('/', [imageUpload.single('image')], async (req: any, res: Response) => {
   if (req.file) {
     //get rid of diacritics, file extension, replace spaces with underscore
@@ -28,11 +21,35 @@ router.post('/', [imageUpload.single('image')], async (req: any, res: Response) 
       .resize(640, 450)
       .toFormat("jpeg")
       .jpeg({ quality: 90 })
-      .toFile(`client/public/images/${normalizedName}.jpg`);
+      .toFile(`client/public/images/${normalizedName}.jpg`)
 
     res.send(`/images/${normalizedName}.jpg`)
   } else {
     res.send('file not present')
+  }
+})
+
+// @route  POST api/images/external/:link
+// @desc   Download image from external link and save it resized
+router.post('/external', async (req: any, res: Response) => {
+  try {
+    const imageLinkSplit = req.body.link.split('/')
+    const imageNameSplit = imageLinkSplit[imageLinkSplit.length - 1].split('.')
+    const imageNameWithTimestamp = `${imageNameSplit[0]}${Date.now()}.${imageNameSplit[1]}`
+    const imagePath = `client/public/images/${imageNameWithTimestamp}`
+    const file = await axios.get(req.body.link, {
+      responseType: 'arraybuffer'
+    })
+
+    await sharp(file.data)
+      .resize(640, 450)
+      .toFormat("jpeg")
+      .jpeg({ quality: 90 })
+      .toFile(imagePath)
+
+    res.send(imagePath)
+  } catch (err) {
+    res.send(err)
   }
 })
 
