96d455b591229bae37cf108342c48db25b69d3dd
Vojtech Vanek <vanevo00@gmail.com>
2020-06-24 09:01:19 +0200

    feat: redirect after flat creation

--- client/pages/add/flat.tsx
+++ client/pages/add/flat.tsx
@@ -6,18 +6,20 @@ import { Flat } from '../../../Types/Flat'
 import FlatForm from '../../components/Forms/FlatForm'
 import AuthContext from '../../context/auth/authContext'
 import { limitToLoggedIn } from '../../utils/limitToLoggedIn'
+import { useRouter } from 'next/router'
 
 const AddFlat = () => {
   const authContext = useContext(AuthContext)
   const [successMessage, setSuccessMessage] = useState('')
+  const router = useRouter()
 
   const onSubmit = async (flat: Flat) => {
     try {
       const data = await axios.post(`${window.location.protocol}//${window.location.hostname}:4000/api/flats/`, flat)
-      setSuccessMessage(`Flat on address ${data.data.address} successfully added`)
+      setSuccessMessage(`Flat on address ${data.data.address} successfully added. Redirecting...`)
       setTimeout(() => {
-        setSuccessMessage('')
-      }, 5000)
+        router.push(`/flat/${data.data._id}`)
+      }, 3000)
     } catch (err) {
       console.log(err)
     }
