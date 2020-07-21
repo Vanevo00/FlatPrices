5055c6d3d6a8b8346bfd3b91c0e3b5f5ca9bfc60
Vojtech Vanek <vanevo00@gmail.com>
2020-07-21 14:16:10 +0200

    feat: widen reas coordinate search

--- client/utils/getReasData.ts
+++ client/utils/getReasData.ts
@@ -4,7 +4,7 @@ import { ReasData } from '../../Types/ReasData'
 
 export default async (searchName, cityName, squareMeters): Promise<ReasData> => {
   try {
-    const sizeSearch = squareMeters < 61 ? '[null, 60]' : '[60, null]'
+    const sizeSearch = squareMeters < 61 ? '[null,60]' : '[60,null]'
 
     const suggestionSearch = await axios.get(`https://apis.reas.cz/atlas/suggestion?query=${searchName}, ${cityName}`)
     const correctArea = suggestionSearch.data.data.find((suggestion) => suggestion.city.toLowerCase().includes(cityName.toLowerCase()))
@@ -31,9 +31,14 @@ export default async (searchName, cityName, squareMeters): Promise<ReasData> => 
       ]
     ] = bounds
 
+    const widenedSouthWestLongitude = southWestLongitude - 0.001
+    const widenedSouthWestLatitude = southWestLatitude - 0.002
+    const widenedNorthEastLongitude = northEastLongitude + 0.001
+    const widenedNorthEastLatitude = northEastLatitude + 0.002
+
     const [medianPriceData, nearbyFlatsData] = await Promise.all([
-      axios.get(`https://apis.reas.cz/atlas/calculator/median-price-per-m?bounds=%7B%22southWestLatitude%22:${southWestLatitude},%22southWestLongitude%22:${southWestLongitude},%22northEastLatitude%22:${northEastLatitude},%22northEastLongitude%22:${northEastLongitude}%7D&types=[%22flat%22]&areaRange=${sizeSearch}`),
-      axios.get(`https://apis.reas.cz/atlas/map-pointers/list?types=[%22flat%22]&limit=10&page=1&areaRange=${sizeSearch}&includeCount=true&bounds=%7B%22southWestLatitude%22:${southWestLatitude},%22southWestLongitude%22:${southWestLongitude},%22northEastLatitude%22:${northEastLatitude},%22northEastLongitude%22:${northEastLongitude}%7D&date=%222018-7-14%22`)
+      axios.get(`https://apis.reas.cz/atlas/calculator/median-price-per-m?bounds=%7B%22southWestLatitude%22:${widenedSouthWestLatitude},%22southWestLongitude%22:${widenedSouthWestLongitude},%22northEastLatitude%22:${widenedNorthEastLatitude},%22northEastLongitude%22:${widenedNorthEastLongitude}%7D&types=[%22flat%22]&areaRange=${sizeSearch}`),
+      axios.get(`https://apis.reas.cz/atlas/map-pointers/list?types=[%22flat%22]&limit=10&page=1&areaRange=${sizeSearch}&includeCount=true&bounds=%7B%22southWestLatitude%22:${widenedSouthWestLatitude},%22southWestLongitude%22:${widenedSouthWestLongitude},%22northEastLatitude%22:${widenedNorthEastLatitude},%22northEastLongitude%22:${widenedNorthEastLongitude}%7D&date=%222018-7-14%22`)
     ])
 
     const medianPricePerMeter = medianPriceData.data.data
